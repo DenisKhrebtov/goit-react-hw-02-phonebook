d2c36af0d5c3d286f71272b59b199cd40f64fc7a
DenisKhrebtov <khrebtovdenis@gmail.com>
2022-10-24 17:11:38 +0300

    add part 2.2

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,23 +1,50 @@
 import React, { Component } from 'react';
-import FormBook from '../FormBook/FormBook';
+import FormBook from '../ContactForm/ContactForm';
+import ContactList from '../ContactList/ContactList';
 
 export class App extends Component {
   state = {
-    contacts: [],
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+
+  deleteContact = contactId => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(todo => todo.id !== contactId),
+    }));
   };
 
   formSubmitHandler = data => {
     console.log(data);
   };
 
+  addContacts = text => {
+    const contact = {
+      id: contact.id,
+      name: contact.name,
+      number: contact.number,
+    };
+
+    this.setState(prevState => ({
+      contacts: [contact, ...prevState.contacts],
+    }));
+  };
+
   render() {
+    const { contacts } = this.state;
+
     return (
       <div>
         <h1>Phonebook</h1>
-        <section>
-          <FormBook onSubmit={this.formSubmitHandler} />
-        </section>
+        <FormBook onSubmit={this.formSubmitHandler} />
         <h2>Contacts</h2>
+        {/* <Filter /> */}
+        <ContactList contacts={contacts} onDeleteContact={this.deleteContact} />
       </div>
     );
   }
---
+++ src/components/ContactElem/ContactElem.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+class ContactEditor extends Component {
+
+    state = {
+        
+    }
+    render() {
+        return (
+
+        )
+    
+        
+    }
+}
